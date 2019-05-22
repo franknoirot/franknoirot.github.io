16b955b5c8d0bf4037f1cd97eaf55b86711cca6a
franknoirot <frank@franknoirot.co>
2019-05-22 03:06:51 -0400

    Accessibility and Cross-Browser Display
    - Added alt attribute to carousel photos
    - Fixed spelling errors

--- _site/assets/js/carousel.js
+++ _site/assets/js/carousel.js
@@ -1,131 +1,209 @@
-const carousels = Array.from(document.querySelectorAll(".carousel"));
+"use strict";
 
-const setup = (carousel) => {
-    const container = carousel.querySelector(".carousel__track-container");
-    const track = carousel.querySelector(".carousel__track");
-    const slides = Array.from(track.children);
-    const nextBtn = carousel.querySelector(".carousel__button--right");
-    const prevBtn = carousel.querySelector(".carousel__button--left");
-    const dotsNav = carousel.querySelector(".carousel__nav");
-    const dots = Array.from(dotsNav.children);
+var carousels = [].slice.call(document.querySelectorAll(".carousel"));
 
-    const setSlidePos = (slides) => {
-        const slideW = slides[0].getBoundingClientRect().width;
+var setup = function setup(carousel) {
+  var container = carousel.querySelector(".carousel__track-container");
+  var track = carousel.querySelector(".carousel__track");
+  var slides = [].slice.call(track.children);
+  var nextBtn = carousel.querySelector(".carousel__button--right");
+  var prevBtn = carousel.querySelector(".carousel__button--left");
+  var dotsNav = carousel.querySelector(".carousel__nav");
+  var dots = [].slice.call(dotsNav.children);
 
-        slides.forEach((slide, i) => {
-            slide.style.left = slideW * i + "px";
-        });
-    };
+  var setSlidePos = function setSlidePos(slides) {
+    var slideW = slides[0].getBoundingClientRect().width;
+    slides.forEach(function (slide, i) {
+      slide.style.left = slideW * i + "px";
+    });
+  };
+
+  setSlidePos(slides);
+
+  var updateCarousel = function updateCarousel(current, target, track) {
+    if (track) track.style.transform = "translateX(-".concat(target.style.left, ")");
+    current.classList.remove('active');
+    target.classList.add('active');
+  };
+
+  var slideRight = function slideRight() {
+    var curSlide = track.querySelector(".active");
+    var nextSlide = curSlide.nextElementSibling ? curSlide.nextElementSibling : slides[0];
+    updateCarousel(curSlide, nextSlide, track);
+    var curDot = dotsNav.querySelector(".active");
+    var nextDot = curDot.nextElementSibling ? curDot.nextElementSibling : dots[0];
+    updateCarousel(curDot, nextDot);
+  };
+
+  nextBtn.addEventListener('click', slideRight); // slide right on nextBtn click
+
+  var slideLeft = function slideLeft() {
+    var curSlide = track.querySelector(".active");
+    var prevSlide = curSlide.previousElementSibling ? curSlide.previousElementSibling : slides[slides.length - 1];
+    updateCarousel(curSlide, prevSlide, track);
+    var curDot = dotsNav.querySelector(".active");
+    var prevDot = curDot.previousElementSibling ? curDot.previousElementSibling : dots[dots.length - 1];
+    updateCarousel(curDot, prevDot);
+  };
+
+  prevBtn.addEventListener('click', slideLeft); // slide left on prevBtn click
+
+  dotsNav.addEventListener('click', function (e) {
+    var targetDot = e.target.closest("button");
+    if (!targetDot) return;
+    var curSlide = track.querySelector('.active');
+    var curDot = dotsNav.querySelector('.active');
+    var targetIndex = dots.findIndex(function (d) {
+      return d === targetDot;
+    });
+    var targetSlide = slides[targetIndex];
+    updateCarousel(curSlide, targetSlide, track);
+    updateCarousel(curDot, targetDot);
+  });
+
+  function keyboardDispatch(e) {
+    if (e.code === "Tab" || e.repeat || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
+    var digit = parseInt(e.code.replace("Digit", "")) - 1;
+
+    if (digit !== NaN && digit >= 0 && digit < slides.length) {
+      var curSlide = track.querySelector('.active');
+      var curDot = dotsNav.querySelector('.active');
+      var targetSlide = slides[digit];
+      var targetDot = dots[digit];
+      updateCarousel(curSlide, targetSlide, track);
+      updateCarousel(curDot, targetDot);
+    }
+
+    switch (e.code) {
+      case "Escape":
+        lightBoxSetup();
+        break;
+
+      case "ArrowLeft":
+        slideLeft();
+        break;
+
+      case "ArrowRight":
+        slideRight();
+    }
+  }
+
+  ;
+
+  function lightBoxSetup() {
+    carousel.classList.toggle('lightbox');
+
+    if (carousel.classList.contains('lightbox')) {
+      document.addEventListener('keydown', keyboardDispatch);
+      track.removeEventListener('keydown', trackKeyCheck);
+    } else {
+      document.removeEventListener('keydown', keyboardDispatch);
+      track.addEventListener('keydown', trackKeyCheck);
+    }
 
     setSlidePos(slides);
+    var curSlide = track.querySelector('.active');
+    updateCarousel(curSlide, curSlide, track);
+  }
 
-    const updateCarousel = (current, target, track) => {
-        if (track) track.style.transform = `translateX(-${target.style.left})`;
-        current.classList.remove('active');
-        target.classList.add('active');
-    };
+  ;
 
-    const slideRight = () => {
-        const curSlide = track.querySelector(".active");
-        const nextSlide = (curSlide.nextElementSibling) ? curSlide.nextElementSibling : slides[0];
-        updateCarousel(curSlide, nextSlide, track);
+  function trackKeyCheck(e) {
+    if (e.code === "Tab" || e.repeat || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
+    lightBoxSetup();
+  }
 
-        const curDot = dotsNav.querySelector(".active");
-        const nextDot = (curDot.nextElementSibling) ? curDot.nextElementSibling : dots[0];
-        updateCarousel(curDot, nextDot);
+  ;
+  track.addEventListener('click', lightBoxSetup);
+  track.addEventListener('keydown', trackKeyCheck); // SWIPE LISTENING
+
+  var swipeX = null;
+  var swipeThresh = 50;
+  track.addEventListener('touchstart', function (e) {
+    swipeX = e.changedTouches[0].pageX;
+  }, false);
+  track.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+  }, false);
+  track.addEventListener('touchend', function (e) {
+    if (!swipeX) return;
+    var dist = swipeX - e.changedTouches[0].pageX;
+
+    if (Math.abs(dist) > swipeThresh) {
+      if (dist >= 0) slideRight();else slideLeft();
     }
-    nextBtn.addEventListener('click', slideRight); // slide right on nextBtn click
-
-    const slideLeft = () => {
-        const curSlide = track.querySelector(".active");
-        const prevSlide = (curSlide.previousElementSibling) ? curSlide.previousElementSibling : slides[slides.length-1];
-        updateCarousel(curSlide, prevSlide, track);
-
-        const curDot = dotsNav.querySelector(".active");
-        const prevDot = (curDot.previousElementSibling) ? curDot.previousElementSibling : dots[dots.length-1];
-        updateCarousel(curDot, prevDot);
-    };
-    prevBtn.addEventListener('click', slideLeft); // slide left on prevBtn click
-
-    dotsNav.addEventListener('click', e => {
-        const targetDot = e.target.closest("button");
-        if (!targetDot) return;
-
-        const curSlide = track.querySelector('.active');
-        const curDot = dotsNav.querySelector('.active');
-        const targetIndex = dots.findIndex(d => d === targetDot);
-        const targetSlide = slides[targetIndex];
-
-        updateCarousel(curSlide, targetSlide, track);
-        updateCarousel(curDot, targetDot);
-    });
-
-    function keyboardDispatch(e) {
-        if (e.code === "Tab" || e.repeat || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
-
-        let digit = parseInt(e.code.replace("Digit", "")) - 1;
-        if (digit !== NaN && digit >= 0 && digit < slides.length) {
-            let curSlide = track.querySelector('.active');
-            let curDot = dotsNav.querySelector('.active');
-            let targetSlide = slides[digit];
-            let targetDot = dots[digit];
-
-            updateCarousel(curSlide, targetSlide, track);
-            updateCarousel(curDot, targetDot);
-        }
-        switch (e.code) {
-            case "Escape":
-                lightBoxSetup();
-                break;
-            case "ArrowLeft":
-                slideLeft();
-                break;
-            case "ArrowRight":
-                slideRight();
-        }
-    };
-
-    function lightBoxSetup() {
-        carousel.classList.toggle('lightbox');
-        if (carousel.classList.contains('lightbox')) {
-            document.addEventListener('keydown', keyboardDispatch);
-            track.removeEventListener('keydown', trackKeyCheck);
-        } else {
-            document.removeEventListener('keydown', keyboardDispatch);
-            track.addEventListener('keydown', trackKeyCheck);
-        }
-        setSlidePos(slides);
-        const curSlide = track.querySelector('.active');
-        updateCarousel(curSlide, curSlide, track);
-    };
-
-    function trackKeyCheck(e) {
-        if (e.code === "Tab" || e.repeat || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
-        lightBoxSetup();
-    };
-
-    track.addEventListener('click', lightBoxSetup);
-    track.addEventListener('keydown', trackKeyCheck);
-
-    // SWIPE LISTENING
-    let swipeX = null;
-    let swipeThresh = 50;
-    track.addEventListener('touchstart', e => {
-        swipeX = e.changedTouches[0].pageX;
-    }, false);
-    track.addEventListener('touchmove', e => {
-        e.preventDefault();
-    }, false);
-    track.addEventListener('touchend', e => {
-        if (!swipeX) return;
-
-        let dist = swipeX - e.changedTouches[0].pageX;
-        if (Math.abs(dist) > swipeThresh) {
-            if (dist >= 0) slideRight();
-            else slideLeft();
-        }
-    },false);
+  }, false);
 };
 
 
+// DO THE DANG THING
 carousels.forEach(setup);
+
+
+
+// POLYFILLS
+// Array.findIndex()
+// https://tc39.github.io/ecma262/#sec-array.prototype.findindex
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value: function(predicate) {
+     // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return k.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return k;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return -1.
+      return -1;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+
+// Element.closest()
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                              Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
--- _site/assets/js/roll-replace.js
+++ _site/assets/js/roll-replace.js
@@ -1,60 +1,57 @@
-const rollReplace = (e) => {
-    e.target.removeEventListener("click", rollReplace);
-    e.target.removeEventListener('keydown', keyCheck);
-    let oldStr = e.target.innerText.split('');
-    let duration = (e.target.getAttribute('data-duration')) ? parseFloat(e.target.getAttribute('data-duration')) : .3;
-    let delay = (e.target.getAttribute('data-delay')) ? parseFloat(e.target.getAttribute('data-delay')) : .1;
-    
-    e.target.innerText = '';
-    
-    oldStr.forEach((letter, i) => {
-      let span = document.createElement('span');
-      span.classList.add('replaced');
+"use strict";
+
+var rollReplace = function rollReplace(e) {
+  e.target.removeEventListener("click", rollReplace);
+  e.target.removeEventListener('keydown', keyCheck);
+  var oldStr = e.target.innerText.split('');
+  var duration = e.target.getAttribute('data-duration') ? parseFloat(e.target.getAttribute('data-duration')) : .3;
+  var delay = e.target.getAttribute('data-delay') ? parseFloat(e.target.getAttribute('data-delay')) : .1;
+  e.target.innerText = '';
+  oldStr.forEach(function (letter, i) {
+    var span = document.createElement('span');
+    span.classList.add('replaced');
+    span.innerHTML = letter.replace(' ', '&nbsp;');
+    e.target.appendChild(span);
+    span.style.animation = "rollUp ".concat(duration, "s ").concat(i * delay, "s ease-in-out forwards"); //
+  });
+  setTimeout(function () {
+    while (e.target.children.length > 0) {
+      e.target.children[0].remove();
+    }
+
+    var newStr = e.target.getAttribute('data-replace').split('');
+    newStr.forEach(function (letter, i) {
+      var span = document.createElement('span');
+      span.classList.add('replacer');
       span.innerHTML = letter.replace(' ', '&nbsp;');
       e.target.appendChild(span);
-      span.style.animation = `rollUp ${duration}s ${i*delay}s ease-in-out forwards`;//
+      span.style.animation = "rollIn ".concat(duration, "s ").concat(i * delay, "s ease-in-out forwards");
     });
-    
-    setTimeout(() => {
-      while(e.target.children.length > 0) {
+    setTimeout(function () {
+      while (e.target.children.length > 0) {
         e.target.children[0].remove();
       }
-      
-      let newStr = e.target.getAttribute('data-replace').split('');
-  
-      newStr.forEach((letter, i) => {
-        let span = document.createElement('span');
-        span.classList.add('replacer');
-        span.innerHTML = letter.replace(' ', '&nbsp;');
-        e.target.appendChild(span);
-        span.style.animation = `rollIn ${duration}s ${i*delay}s ease-in-out forwards`;
-      });
-      
-      setTimeout(() => {
-        while(e.target.children.length > 0) {
-          e.target.children[0].remove();
-        }
-  
-        e.target.innerText = newStr.join('');
-        e.target.setAttribute('data-replace', oldStr.join(''));
-        
-        e.target.addEventListener("click", rollReplace);
-        e.target.addEventListener('keydown', keyCheck);
-      }, newStr.length*500*duration);
-    }, oldStr.length*500*duration);
-  }
-  
-  function keyCheck(e) {
-    if (e.code === "Tab" || e.repeat || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
-    e.preventDefault();
-    rollReplace(e);
-  }
 
-  window.addEventListener('DOMContentLoaded', () => {
-    let roll = document.querySelector('.roll-replace');
-    if (roll) {
-      roll.tabIndex = 0;
-      roll.addEventListener('click', rollReplace);
-      roll.addEventListener('keydown', keyCheck);
-    }
-  });
+      e.target.innerText = newStr.join('');
+      e.target.setAttribute('data-replace', oldStr.join(''));
+      e.target.addEventListener("click", rollReplace);
+      e.target.addEventListener('keydown', keyCheck);
+    }, newStr.length * 500 * duration);
+  }, oldStr.length * 500 * duration);
+};
+
+function keyCheck(e) {
+  if (e.code === "Tab" || e.repeat || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
+  e.preventDefault();
+  rollReplace(e);
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  var roll = document.querySelector('.roll-replace');
+
+  if (roll) {
+    roll.tabIndex = 0;
+    roll.addEventListener('click', rollReplace);
+    roll.addEventListener('keydown', keyCheck);
+  }
+});
